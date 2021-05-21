857a2374157fb25029e03e51d868b0084b227679
chikage4 <chikagekondo@gmail.com>
2021-05-21 21:57:53 +0900

    first upload

---
+++ storeAction.php
@@ -0,0 +1,57 @@
+<?php
+  require_once "classes/Store.php";
+
+  $store = new Store();
+
+ 
+
+  if(isset($_POST['register'])){
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $address = $_POST['address'];
+    $email = $_POST['email'];
+    $contact_number = $_POST['contact_number'];
+    $username = $_POST['username'];
+    $password =md5($_POST['password']);
+
+    $create_account = $store->createAccount($username,$password);
+
+    if($create_account == true){
+        $user->createUser($first_name,$last_name,$address,$email,$contact_number);
+    }
+
+  }elseif(isset($_POST['login'])){
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+
+    $user->login($username, $password);
+  }elseif(isset($_POST['submit'])){
+    $item_name = $_POST['product_name'];
+    $Price = $_POST['price'];
+    $description = $_POST['description'];
+  
+    $add_item = $store->add_item($item_name,$Price,$description);
+  }elseif(isset($_POST['update'])){
+    $item_name = $_POST['item_name'];
+    $Price = $_POST['item_price'];
+    $description = $_POST['item_description'];
+
+    $get_items = $store->get_items($item_name,$Price,$description);
+  }elseif(isset($_POST['add_to_cart'])){
+    $product_name = $_POST['item_name'];
+    $product_price = $_POST['item_price'];
+    $user_id = $_POST['user_id'];
+
+    $add_to_cart = $store->add_to_cart($product_name,$product_price,$user_id);
+  }elseif(isset($_POST['see_cart'])){
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $user_id = $_POST['user_id'];
+
+    $see_cart = $store->see_cart($product_name,$product_price,$user_id);
+  }
+
+
+?>
+
+
